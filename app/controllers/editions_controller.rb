25846b22b66c970fcaf44a9e73a38afbc43256ec
Tarek Alatrach <talatrach@gmail.com>
2026-05-10 18:28:47 -0600

    refactor: remove index action from EditionsController and update routes to only include show action

--- app/controllers/editions_controller.rb
+++ app/controllers/editions_controller.rb
@@ -1,11 +1,6 @@
 class EditionsController < ApplicationController
-  def index
-    @edition = @newspaper.editions.find(params[:id])
-    @stories = @edition.stories
-  end
-
   def show
-    @edition = Edition.includes(:stories).find(params[:id])
+    @edition = Edition.includes(:newspaper).find(params[:id])
     @stories = @edition.stories.order(:position)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   resources :newspapers, only: [] do
-    resources :editions, only: [:index, :show]
+    resources :editions, only: [:show]
   end
 
   root "newspapers#index"  
